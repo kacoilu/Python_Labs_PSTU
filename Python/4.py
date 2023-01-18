1cdddd1276c8783b469e6bd56b3bca09e63ef9c6
Danila <118891007+kacoilu@users.noreply.github.com>
2023-01-18 15:04:09 +0500

    Update 4.py

--- Python/4.py
+++ Python/4.py
@@ -4,7 +4,10 @@ from sklearn import datasets
 
 #1 на базе данных введенных вручную
 my_list = [int(el) for el in "1 2 3 4 5 6 7 8 9".split()]
+my_list_squared = map(lambda num: num +3*4, my_list)
+
 print(my_list)
+print(list(my_list_squared))
 
 #2 на базе данных полученных при парсинге
 URL = "https://realpython.github.io/fake-jobs/"
